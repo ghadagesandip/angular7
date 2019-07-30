aa9cb3d9b4f1b3cb38040f8050daa44c7e10575d
Sandip Ghadge <sandip.ghadge@wwindia.com>
2019-07-30 14:01:34 +0530

    updated coed

--- src/app/shared/pipes/charlimit.pipe.ts
+++ src/app/shared/pipes/charlimit.pipe.ts
@@ -8,7 +8,7 @@ export class CharlimitPipe implements PipeTransform {
   constructor() {
     console.log('pipe loaded');
   }
-  transform(value: string, limit: number = 300): string {
+  transform(value: string, limit: number = 500): string {
     console.log('called pipe');
     if (value.length > limit) {
       return `${value.slice(0, limit)} ...`;
