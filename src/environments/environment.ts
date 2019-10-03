b6ca2336dd4842a5624fddbf7f7880d04229c4b4
Sandip Ghadge <sandip.ghadge@wwindia.com>
2019-10-03 12:01:45 +0530

    updated env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://sandip-shopping-app.herokuapp.com/api/',
-   apiUrl: 'http://localhost:3030/api/',
+  // apiUrl: 'http://localhost:3030/api/',
+  apiUrl : 'http://enigmatic-reef-16960.herokuapp.com/api/',
   debugMode: true
 };
 
